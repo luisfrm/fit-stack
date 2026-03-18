a42ef0b57a0626c2e2238a8036980b690b9d2b54
Luis Rivas <luisfrm_@outlook.com>
2026-03-18 05:43:54 -0400

    feat: add supresswarning to body generated by grammarly extension

--- apps/cms/app/layout.tsx
+++ apps/cms/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning>
       <body
+        suppressHydrationWarning
         className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased bg-background text-foreground`}
       >
         {children}
