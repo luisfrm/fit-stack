9a8cf5474d34b4d644bfbf397ebdcef7b036298d
Luis Rivas <luisfrm_@outlook.com>
2026-03-18 05:19:47 -0400

    feat: create health route on api

--- apps/api/app/api/health/route.ts
+++ apps/api/app/api/health/route.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  console.log(process.env.NEON_AUTH_BASE_URL);
+  console.log(process.env.NEON_AUTH_COOKIE_SECRET);
+  console.log(process.env.DATABASE_URL);
   return NextResponse.json({
     status: "ok",
     message: "API is running",
